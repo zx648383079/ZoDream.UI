e226866559fd8056c498b9cfe8eeabe8a9ec2a35
Jason Zou <jason.zou@nwayschina.com>
2015-10-13 17:16:03 +0800

    修复

--- js/zodream.js
+++ js/zodream.js
@@ -9,14 +9,14 @@ zodream.prototype = {
 	init: function(name) {
 		if(typeof name === "string")
 		{
-			this.elements = this.getMore(name);
+			this.elements = this.getMore( name , arguments[1] || window.document);
 		}else {
 			this.elements = [name];
 		}
 	},
 	getMore: function(name) {
-		var names = name.split(",");
-		var elements = Array();
+		var names = name.split(","),
+			elements = Array();
 		for (var i = 0, len = names.length; i < len; i++) {
 			Array.prototype.push.apply(elements, this.getNext( names[i], arguments[1] || window.document ) );
 		};
@@ -40,7 +40,7 @@ zodream.prototype = {
 		switch (name.charAt(0)) {
 			case '.':
 				name = name.slice(1);
-				return this.getElementByClass(name, parent);
+				return this.getChildByClass(name, parent);
 				break;
 			case '#':
 				name = name.slice(1);
@@ -48,16 +48,32 @@ zodream.prototype = {
 				break;
 			case '@':
 				name = name.slice(1);
-				return parent.getElementsByName(name);
+				return window.document.getElementsByName(name);
+				break;
+			case '$':
+				name = name.slice(1);
+				return this.getChildByIndex( Number(name), parent);
 				break;
 			default:
 				return parent.getElementsByTagName(name);
 				break;
 		}
 	},
-	getElementByClass: function(name) {
-		var parent = arguments[1] || window.document;
-		var elements = parent.getElementsByTagName("*"),
+	getChildByIndex: function(index) {
+		var parent = arguments[1] || window.document,
+			elements = parent.getElementsByTagName("*");
+		for (var i = 0, len = elements.length; i < len; i++) {
+			if(elements[i].nodeType == 1) {
+				index --;
+				if(index < 0) {
+					return elements[i];
+				}
+			}
+		}
+	},
+	getChildByClass: function(name) {
+		var parent = arguments[1] || window.document,
+			elements = parent.getElementsByTagName("*"),
 			classElements = Array();
 		for (var i = 0, len = elements.length; i < len; i++) {
 			var element = elements[i];
@@ -68,18 +84,6 @@ zodream.prototype = {
 			}
 		};
 		return classElements;
-	},
-	extend: function() {
-		for (var i = 0,len = arguments.length; i < len; i++) {
-			var arg = arguments[i];
-			if(typeof arg === "object") {
-				for (var key in arg) {
-					if (arg.hasOwnProperty(key)) {
-						this[key] = arg[key];
-					}
-				};
-			}
-		};
 	},
 	attr: function(name) {
 		if(arguments[1] === undefined) {
@@ -116,7 +120,7 @@ zodream.prototype = {
 		}
 	},
 	show: function() {
-		this.css("display", "block");	
+		this.css("display", "block");
 	},
 	hide: function() {
 		this.css("display", "none");		
@@ -130,9 +134,6 @@ zodream.prototype = {
 			}, arguments[0]);
 		}
 	},
-	text: function() {
-		
-	},
 	val: function() {
 		if(arguments[0] === undefined) {
 			return this.elements[0].value;
@@ -143,10 +144,14 @@ zodream.prototype = {
 		}
 	},
 	getForm: function() {
-		var data = "";
-		var elements = this.getMore('input,textarea', this.elements[0]);
+		var data = "",
+			elements = this.getMore('input,textarea', this.elements[0]);
 		for (var i = 0, len = elements.length; i < len; i++) {
 			var element = elements[i];
+			if(element.required && element.value == "") {
+				element.style.border = "1px solid red";
+				return false;
+			};
 			switch (element.type.toLowerCase()) {    
 				case 'submit':
 					break;
@@ -154,8 +159,10 @@ zodream.prototype = {
 				case 'password':    
 				case 'text':
 				case 'email':
+					data += "&" + element.name + "=" + Helper.encode(element.value);
+					break; 
 				case 'textarea':
-					data += "&" + element.name + "=" + element.value;
+					data += "&" + element.name + "=" + Helper.encode( Helper.toHtml(element.value) );
 					break; 
 				case 'checkbox':    
 				case 'radio':
@@ -193,14 +200,13 @@ zodream.prototype = {
 			}
 		}
 	},
-	forE: function() {
-		var args = Array.prototype.slice.call(arguments);
-		var func = args.shift();
+	forE: function(func) {
 		var data = Array();
 		if(typeof func === "function") {
 			for (var i = 0, len = this.elements.length; i < len; i++) {
-				args.unshift(this.elements[i], i );
-				data.push(func.apply(null, args));
+				var args = Array.prototype.slice.call(arguments, 1);
+				args.unshift( this.elements[i], i );
+				data.push( func.apply( null, args) );
 			};
 		}
 		return data;
@@ -232,11 +238,12 @@ zodream.prototype = {
 	},
 	remove: function() {
 		for (var i = 0, len = arguments.length; i < len; i++) {
-			this.forE(function(e, j, ele) {
-				if(e == ele) {
-					this.elements.splice( j, 1 );
+			for (var j = 0; j < this.elements.length; j++) {
+				if(this.elements[j] == arguments[i])
+				{
+					this.elements.splice( j , 1);
 				}
-			}, arguments[i]);
+			};
 		};
 	},
 	removeAt: function() {
@@ -246,15 +253,15 @@ zodream.prototype = {
 		}
 	},
 	addEvent: function() {
-		var event, fun, func,args;
-		args = Array.prototype.slice.call(arguments);
-		event = args.shift();
-		func = fun = args.shift();
+		var args = Array.prototype.slice.call(arguments),
+			event = args.shift(),
+			fun = args.shift(),
+			func = fun;
 		if(args.lenght > 0)
 		{
 			func = function(e)
 			{
-				fun.apply(this, arguments);  //继承监听函数,并传入参数以初始化;
+				fun.apply( this, arguments);  //继承监听函数,并传入参数以初始化;
 			}
 		};
 		
@@ -294,6 +301,15 @@ zodream.prototype = {
 
 var Helper = {
 	ajax: {
+		default: {
+			method: "GET",
+			url: '',
+			data: null,
+			success: null,
+			error: null,
+			async: true
+		},
+		settings: {},
 		http: null,
 		getHttp: function() {
 			if(window.ActiveXObject) {
@@ -307,29 +323,62 @@ var Helper = {
 				this.http = new XMLHttpRequest();
 			}
 		},
-		request: function(method, url, msg, async) {
-			this.getHttp();
-			this.responseCallback = null;
-			this.http.open( url?"GET":method, url || method, async || true);  
-			this.http.onreadystatechange = this.response;  
-			this.http.send( msg );  
+		request: function() {
+			this.http.open( this.settings.method , this.settings.url, this.settings.async);  
+			this.http.onreadystatechange = this.response.bind(this); 
+			this.http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+			this.http.send( this.settings.data );  
 		},
 		response: function() {
-			if (this.http.readyState == 4) {  
+			if (this.http.readyState == 4) { 
 				if (this.http.status == 200) {  
 					//var text = decodeURI( this.http.responseText );
+					var data;
+					try {
+						data = Helper.parseJSON(this.http.responseText );
+					} catch (error) {
+						data = this.http.responseText;
+					}
+					this.settings.success( data , this.http );
+				}else {
+					this.settings.error(this.http.responseText, this.http.status, this.http);
 				}
-				this.responseCallback(this.http.responseText, this.http.status, this.http);
 			}
 		},
-		responseCallback: null,
 		get: function(url, func) {
-			this.request(url);
-			this.responseCallback = func;
+			var data;
+			if(arguments.length > 1)
+			{
+				data = {
+					url: arguments[0],
+					success: arguments[1],
+				};
+			}else {
+				data = arguments[0];
+			}
+			this.load(data);
 		},
-		post: function(url, data, func) {
-			this.request("POST", url , data );
-			this.responseCallback = func;
+		post: function() {
+			var data;
+			if(arguments.length > 1)
+			{
+				data = {
+					method: "POST",
+					url: arguments[0],
+					data: arguments[1],
+					success: arguments[2],
+				};
+			}else {
+				data = arguments[0];
+			}
+			if( data.data === false) return;
+			this.load(data);
+			
+		},
+		load: function( data ) {
+			Helper.extend.call( this.settings , this.default, data);
+			this.getHttp();
+			this.request();
 		}
 	},
 	date: {
@@ -346,6 +395,81 @@ var Helper = {
 			}
 			return num;
 		}
+	},
+	extend: function() {
+		for (var i = 0,len = arguments.length; i < len; i++) {
+			var arg = arguments[i];
+			if(typeof arg === "object") {
+				for (var key in arg) {
+					if (arg.hasOwnProperty(key)) {
+						this[key] = arg[key];
+					}
+				};
+			}
+		};
+	},
+	forE: function(func) {
+		var data = Array();
+		if(typeof func === "function") {
+			for (var i = 0, len = this.length; i < len; i++) {
+				var args = Array.prototype.slice.call(arguments, 1);
+				args.unshift( this[i], i );
+				data.push( func.apply( null, args) );
+			};
+		}
+		return data;
+	},
+	clone: function(obj) { 
+        var o;  
+		switch(typeof obj){  
+			case 'undefined': 
+				break;  
+			case 'string'   : 
+				o = obj + '';
+				break;  
+			case 'number'   : 
+				o = obj - 0;
+				break;  
+			case 'boolean'  : 
+				o = obj;
+				break;  
+			case 'object'   :  
+				if(obj === null){  
+					o = null;  
+				}else{  
+					if(obj instanceof Array){  
+						o = [];  
+						for(var i = 0, len = obj.length; i < len; i++){  
+							o.push(this.clone(obj[i]));  
+						}  
+					}else{  
+						o = {};  
+						for(var k in obj){  
+							o[k] = this.clone(obj[k]);  
+						}  
+					}  
+				}  
+				break;  
+			default:          
+				o = obj;
+				break;  
+		}  
+		return o; 
+	},
+	parseJSON: function( data ) {
+		return JSON.parse( data + "" );
+	},
+	refresh: function() {
+		window.location.reload();
+	},
+	htmlTo: function(data) {
+		return data.replace(/(&nbsp;)/g, " ").replace(/\<br\>/g, "\r\n");
+	},
+	toHtml: function(data) {
+		return data.replace(/[ ]/g, "&nbsp;").replace(/\n|\r|(\r\n)|(\u0085)|(\u2028)|(\u2029)/g, "<br>");
+	},
+	encode: function(data){  
+		return encodeURI(data).replace(/&/g, '%26').replace(/\+/g,'%2B').replace(/\s/g,'%20').replace(/#/g,'%23');  
 	}
 };
 

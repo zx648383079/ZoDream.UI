2c44d3b01ce3fe75deb1f6ffa77375d785ed69bd
zx648383079 <648383079@qq.com>
2016-08-13 23:51:22 +0800

    上传手机页面初稿

--- js/zodream.js
+++ js/zodream.js
@@ -1,1 +1,10 @@
+$(document).ready(function () {
+    $(".expand .title").click(function () {
+        $(this).parent().toggleClass("active");
+    });
+    $(".tab .headers>li").click(function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        $(this).parent().parent().find(".items>li").eq($(this).index()).addClass("active").siblings().removeClass("active");
+    });
+});
 //# sourceMappingURL=zodream.js.map

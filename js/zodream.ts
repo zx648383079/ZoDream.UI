895ba979b3aa4c690a42d86d2fcd1125f9398ada
zx648383079 <648383079@qq.com>
2016-09-02 15:59:07 +0800

    增加EDGE 通知

--- js/zodream.ts
+++ js/zodream.ts
@@ -1,3 +1,17 @@
+if ("Notification" in window) {
+    let ask = Notification.requestPermission();
+    ask.then( permission => {
+        if ( permission === "granted") {
+            let msg = new Notification("Title", {
+                body: "",
+                icon: "",
+            });
+            msg.addEventListener("click", event => {
+                alert("");
+            })
+        }
+    });
+}
 $(document).ready(function() {
     $(".expand .title").click(function() {
         $(this).parent().toggleClass("active");

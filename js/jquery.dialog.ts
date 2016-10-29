5c189f924d18d2547134960fadeda6fd4ce9b9fb
zx648383079 <648383079@qq.com>
2016-10-29 11:38:23 +0800

    修改

--- js/jquery.dialog.ts
+++ js/jquery.dialog.ts
@@ -1,4 +1,35 @@
 class Dialog {
+    constructor(
+        public element: JQuery,
+        option?: DialogOptions
+    ) {
+        this.option = $.extend({}, new DialogDefaultOptions, option);
+    }
+
+    public option: DialogOptions;
+
+    public addMoveEvent() {
+        let iDiffX: number,iDiffY: number, instance = this;
+        this.element.find(this.option.titleTag).mousedown(function(e) {
+            $(this).css({"cursor":"move"});
+            iDiffX = e.pageX - $(this).offset().left;
+            iDiffY = e.pageY - $(this).offset().top;
+            $(document).mousemove(function(e){
+                instance.element.css({"left":(e.pageX - iDiffX),"top":(e.pageY - iDiffY)});
+            });	
+        }).mouseup(function() {
+            $(document).unbind("mousemove");
+		    $(this).css({"cursor":"auto"});
+        });
+    }
+
+    public addCloseEvent() {
+        let instance = this;
+        this.element.find(this.option.closeTag).click(function() {
+            instance.element.hide();
+        });
+    }
+
     public static tip(content: string, time: number = 2000) {
         let element = $(".tipDialog");
         element.html(content).show();
@@ -23,6 +54,22 @@ class Dialog {
     }
 }
 
-;(function($: any) {
-  $.dialog = Dialog;
+interface DialogOptions {
+     titleTag?: string,
+     minTag?: string,
+     closeTag?: string,
+ }
+
+ class DialogDefaultOptions implements DialogOptions {
+     titleTag: string = '.head';
+     minTag: string = '.min';
+     closeTag: string = '.close';
+ }
+ 
+ ;(function($: any) {
+
+  $.fn.dialog = function(options ?:DialogOptions) {
+    return new Dialog(this, options); 
+  };
+  
 })(jQuery);

605b8882ff6a4214003a62c6480acdb36d84ed5f
zx648383079 <648383079@qq.com>
2016-11-06 22:08:34 +0800

    增加可视化编辑

---
+++ js/ui.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $("#weight .grid").draggable({
+        connectToSortable: ".row",
+        helper: "clone",
+        opacity: .3,
+        revert: "invalid"
+    });
+    $("#main .row").sortable({
+        connectWith: ".row"
+    });
+    $(".panel .fa-close").click(function () {
+        $(this).parent().parent().addClass("min");
+    });
+    $(".panel>.head>.title").click(function () {
+        var panel = $(this).parent().parent();
+        if (panel.hasClass("min")) {
+            panel.removeClass("min");
+        }
+    });
+    $.htmlClean($("#main").html(), {
+        format: true,
+        allowedAttributes: [
+            ["id"],
+            ["class"],
+            ["data-toggle"],
+            ["data-target"],
+            ["data-parent"],
+            ["role"],
+            ["data-dismiss"],
+            ["aria-labelledby"],
+            ["aria-hidden"],
+            ["data-slide-to"],
+            ["data-slide"]
+        ]
+    });
+});
+//# sourceMappingURL=ui.js.map

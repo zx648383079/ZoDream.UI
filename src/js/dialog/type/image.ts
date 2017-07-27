808eb454af4a2efda2d3b3dc1fe287a58c97f8c3
zx648383079 <648383079@qq.com>
2017-07-27 14:08:55 +0800

    修改图片弹出框自适应

--- src/js/dialog/type/image.ts
+++ src/js/dialog/type/image.ts
@@ -14,6 +14,8 @@ class DialogImage extends DialogContent {
 
     private _index: number = 0;
 
+    private _img: JQuery;
+
     private _src: string;
 
     public get src(): string {
@@ -25,23 +27,56 @@ class DialogImage extends DialogContent {
             img = this.options.content;
         }
         this._src = img;
-        this.box.find('.dialog-body img').attr('src', img);
+        this._img.attr('style', '').attr('src', img);
     }
 
     protected createContent(): this {
         this.box.html(this.getContentHtml());
+        this._img = this.box.find('.dialog-body img');
         return this;
     }
 
     protected setProperty(): this {
         let target = this.options.target || Dialog.$window;
         let maxWidth = target.width();
-        let width = this.box.width();
+        let width = this._img.width();
         let maxHeight = target.height();
-        let height = this.box.height();
+        let height = this._img.height();
+        if (width <= maxWidth && height <= maxHeight) {
+            this.css({
+                left: (maxWidth - width) / 2 + 'px',
+                top: (maxHeight - height) / 2 + 'px',
+                height: height,
+                width: width
+            });
+            return this;
+        }
+        let wScale = width / maxWidth;
+        let hScale = height / maxHeight;
+        if (wScale >= hScale) {
+            height /= wScale; 
+            this.css({
+                left: 0,
+                top: (maxHeight - height) / 2 + 'px',
+                height: height,
+                width: maxWidth
+            });
+            this._img.css({
+                height: height,
+                width: maxWidth
+            });
+            return this;
+        }
+        width /= hScale;
         this.css({
             left: (maxWidth - width) / 2 + 'px',
-            top: (maxHeight - height) / 2 + 'px'
+            top: 0,
+            height: maxHeight,
+            width: width
+        });
+        this._img.css({
+            height: height,
+            width: maxWidth
         });
         return this;
     }

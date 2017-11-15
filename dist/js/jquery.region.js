acd5255353621adc0e1e0e35a3f5019b810ca155
zx648383079 <648383079@qq.com>
2017-11-15 19:23:56 +0800

    增加联动选择

--- dist/js/jquery.region.js
+++ dist/js/jquery.region.js
@@ -1,10 +1,13 @@
 var Region = (function () {
     function Region(element, option) {
+        var _this = this;
         this.element = element;
+        this.selectList = [];
         this.option = $.extend({}, new RegionDefaultOption(), option);
-        this.provEle = this.element.find('.prov');
-        this.cityEle = this.element.find('.city');
-        this.distEle = this.element.find('.dist');
+        this.selectList = [];
+        this.element.find('select').each(function (i, ele) {
+            _this.selectList.push($(ele));
+        });
         if (typeof this.option.data == 'object') {
             this.init();
             return;
@@ -12,42 +15,112 @@ var Region = (function () {
         var instance = this;
         $.getJSON(this.option.data, function (data) {
             if (data.code == 0) {
-                instance.option.data = data.data[1] ? data.data[1]['children'] : data.data;
+                instance.option.data = data.data;
                 instance.init();
             }
         });
     }
+    Region.prototype.map = function (callback, start) {
+        if (start === void 0) { start = 0; }
+        var count = this.selectList.length;
+        for (; start < count; start++) {
+            if (false == callback(this.selectList[start], start, count)) {
+                return;
+            }
+        }
+    };
     Region.prototype.init = function () {
-        this.initProv();
+        this.showOption(this.selectList[0], 0);
         var instance = this;
-        this.provEle.change(function () {
-            instance.initCity();
+        this.map(function (item, i, count) {
+            if (i >= count - 1) {
+                return false;
+            }
+            item.change(function () {
+                var element = instance.eq(i + 1);
+                var val = element.val();
+                if (val) {
+                    element.attr('data-value', val);
+                }
+                instance.showOption(element, i + 1);
+            });
         });
-        this.cityEle.change(function () {
-            instance.initDist();
+    };
+    Object.defineProperty(Region.prototype, "val", {
+        get: function () {
+            var args = [];
+            this.map(function (item) {
+                args.push(item.val());
+            });
+            return args;
+        },
+        set: function (args) {
+            this.map(function (item, i) {
+                item.attr('data-value', args[i]);
+            });
+            this.showOption(this.selectList[0], 0);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Region.prototype.eq = function (i) {
+        if (i < 0) {
+            i = 0;
+        }
+        if (this.selectList.length <= i) {
+            i = this.selectList.length - 1;
+        }
+        return this.selectList[i];
+    };
+    Region.prototype.getOptionData = function (i) {
+        if (i < 1) {
+            return this.option.data;
+        }
+        var data = this.option.data;
+        var id;
+        this.map(function (item, index) {
+            if (index >= i) {
+                return false;
+            }
+            id = item.val();
+            if (!data.hasOwnProperty(id)) {
+                data = null;
+                return false;
+            }
+            data = data[id]['children'];
         });
+        return data;
     };
-    Region.prototype.initProv = function () {
-        this.provEle.html(this.getSelectHtml(this.option.data));
-        this.cityEle.html(this.getSelectHtml({}));
-        this.distEle.html(this.getSelectHtml({}));
+    Region.prototype.getOptionTip = function (i) {
+        if (typeof this.option.tips == 'object') {
+            return this.option.tips[i];
+        }
+        return this.option.tips;
     };
-    Region.prototype.initCity = function () {
-        var id = this.provEle.val();
-        this.cityEle.html(this.getSelectHtml(this.option.data[id]['children']));
-        this.distEle.html(this.getSelectHtml({}));
+    Region.prototype.showOption = function (element, i) {
+        var _this = this;
+        var hasSelected = false;
+        element.html(this.getSelectHtml(this.getOptionData(i), element.attr('data-value'), this.getOptionTip(i), function () {
+            hasSelected = true;
+        }));
+        this.map(function (item, index) {
+            item.html(_this.getSelectHtml({}, null, _this.getOptionTip(index)));
+        }, i + 1);
+        if (hasSelected) {
+            element.trigger('change');
+        }
     };
-    Region.prototype.initDist = function () {
-        var prov = this.provEle.val();
-        var city = this.cityEle.val();
-        this.distEle.html(this.getSelectHtml(this.option.data[prov]['children'][city]['children']));
-    };
-    Region.prototype.getSelectHtml = function (data, selected, defaultLabel) {
+    Region.prototype.getSelectHtml = function (data, selected, defaultLabel, selectedCallback) {
         if (defaultLabel === void 0) { defaultLabel = '请选择'; }
         var html = '<option value="">' + defaultLabel + '</option>';
+        if (!data) {
+            return html;
+        }
         var instance = this;
         $.each(data, function (id, item) {
-            html += instance.getOptionHtml(id, item.name, id == selected);
+            var isSelected = id == selected;
+            html += instance.getOptionHtml(id, item.name, isSelected);
+            isSelected && selectedCallback && selectedCallback();
         });
         return html;
     };
@@ -63,6 +136,7 @@ var Region = (function () {
 var RegionDefaultOption = (function () {
     function RegionDefaultOption() {
         this.data = 'region/tree';
+        this.tips = '请选择';
     }
     return RegionDefaultOption;
 }());
--- src/js/jquery.region.ts
+++ src/js/jquery.region.ts
@@ -4,9 +4,10 @@
          option?: RegionOption
      ) {
          this.option = $.extend({}, new RegionDefaultOption(), option);
-         this.provEle = this.element.find('.prov');
-         this.cityEle = this.element.find('.city');
-         this.distEle = this.element.find('.dist');
+         this.selectList = [];
+         this.element.find('select').each((i, ele) => {
+             this.selectList.push($(ele));
+         })
          if (typeof this.option.data == 'object') {
              this.init();
              return;
@@ -14,7 +15,7 @@
          let instance = this;
          $.getJSON(this.option.data, function(data) {
             if (data.code == 0) {
-                instance.option.data = data.data[1] ? data.data[1]['children'] : data.data;
+                instance.option.data = data.data;
                 instance.init();
             }
          });
@@ -22,48 +23,116 @@
 
      public option: RegionOption;
 
-     public provEle: JQuery;
+     public selectList: Array<JQuery> = [];
 
-     public cityEle: JQuery;
-
-     public distEle: JQuery;
-
-
+     public map(callback: (ele: JQuery, i: number, count: number) => any, start: number = 0) {
+        let count = this.selectList.length;
+        for (;start < count; start ++) {
+            if (false == callback(this.selectList[start], start, count)) {
+                return;
+            }
+        }
+     }
 
      public init() {
-        this.initProv();
+        this.showOption(this.selectList[0], 0);
         let instance = this;
-        this.provEle.change(function() {
-            instance.initCity();
+        this.map((item, i, count) => {
+            if (i >= count - 1) {
+                return false;
+            }
+            item.change(function() {
+                let element = instance.eq(i + 1);
+                let val = element.val();
+                if (val) {
+                    element.attr('data-value', val);
+                }
+                instance.showOption(element, i + 1);
+            });
         });
-        this.cityEle.change(function() {
-            instance.initDist();
+     }
+
+     
+     public set val(args: Array<string|number>) {
+        this.map((item, i) => {
+            item.attr('data-value', args[i]);
         });
+        this.showOption(this.selectList[0], 0);
      }
 
-     public initProv() {
-         this.provEle.html(this.getSelectHtml(this.option.data));
-         this.cityEle.html(this.getSelectHtml({}));
-         this.distEle.html(this.getSelectHtml({}));
+     
+     public get val(): Array<string|number> {
+        let args = [];
+        this.map(item => {
+            args.push(item.val());
+        });
+        return args;
      }
 
-     public initCity() {
-         let id = this.provEle.val();
-         this.cityEle.html(this.getSelectHtml(this.option.data[id]['children']));
-         this.distEle.html(this.getSelectHtml({}));
+     
+
+     public eq(i: number): JQuery {
+        if (i < 0) {
+            i = 0;
+        }
+        if (this.selectList.length <= i) {
+            i = this.selectList.length - 1;
+        }
+        return this.selectList[i];
+     }
+     
+     
+
+     public getOptionData(i: number): any {
+        if (i < 1) {
+            return this.option.data;
+        }
+        let data = this.option.data;
+        let id: string;
+        this.map((item, index) => {
+            if (index >= i) {
+                return false;
+            }
+            id = item.val();
+            if (!data.hasOwnProperty(id)) {
+                data = null;
+                return false;
+            }
+            data = data[id]['children'];
+        });
+        return data;
      }
 
-    public initDist() {
-         let prov = this.provEle.val();
-         let city = this.cityEle.val();
-         this.distEle.html(this.getSelectHtml(this.option.data[prov]['children'][city]['children']));
+     public getOptionTip(i: number): string {
+         if (typeof this.option.tips == 'object') {
+             return this.option.tips[i];
+         }
+         return this.option.tips;
      }
 
-    public getSelectHtml(data: any, selected?: number, defaultLabel: string = '请选择'): string {
+     public showOption(element: JQuery, i: number) {
+        let hasSelected = false; 
+        element.html(this.getSelectHtml(this.getOptionData(i), element.attr('data-value'), this.getOptionTip(i), () => {
+            hasSelected = true;
+        }));
+        this.map((item, index) => {
+            item.html(this.getSelectHtml({}, null, this.getOptionTip(index)));
+        }, i + 1);
+        if (hasSelected) {
+            element.trigger('change');
+        }
+     }
+
+    public getSelectHtml(data: any, selected?: number | string, defaultLabel: string = '请选择', selectedCallback?: Function): string {
         let html = '<option value="">'+ defaultLabel +'</option>';
+        if (!data) {
+            return html;
+        }
         let instance = this;
         $.each(data, function(id: number, item) {
-            html += instance.getOptionHtml(id, item.name, id == selected);
+            let isSelected = id == selected;
+            html += instance.getOptionHtml(id, item.name, isSelected);
+            isSelected && selectedCallback && selectedCallback();
         });
         return html;
      }
@@ -77,11 +146,13 @@
 }
 
 interface RegionOption {
-    data?: any
+    data?: any,
+    tips?: string | Array<string>
 }
 
 class RegionDefaultOption implements RegionOption  {
     data: string = 'region/tree';
+    tips: string = '请选择';
 }
  
  ;(function($: any) {

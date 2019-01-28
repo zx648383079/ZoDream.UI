bab29746669d0e38fd68bdc59d46319b54984d2f
zodream <648383079@qq.com>
2019-01-28 09:26:29 +0800

    修改

---
+++ src/js/editor/editor.ts
@@ -0,0 +1,20 @@
+class Editor {
+    
+    constructor(
+        element: any
+    ) {
+        
+    }
+
+
+
+
+
+
+    /**
+     * create
+     */
+    public static create(element: any) {
+        return new Editor(element);
+    }
+}
